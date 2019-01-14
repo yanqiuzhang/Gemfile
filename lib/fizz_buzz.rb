fc8974d5f621dd8de2c5bb231c2e6c135dd7d155
olivia <olivia@evolve.se>
2019-01-14 10:47:38 +0100

    commit files

---
+++ lib/fizz_buzz.rb
@@ -0,0 +1,17 @@
+def fizz_buzz(number)
+    if has_zero_remainder?(number, 15)
+        'fizz buzz'
+      elsif has_zero_remainder?(number, 5)
+        'buzz'
+      elsif has_zero_remainder?(number, 3)
+        'fizz'
+      else
+        number
+      end
+end
+
+def has_zero_remainder?(number, divider)
+    number % divider == 0
+end
+
+
---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,20 @@
+require './lib/fizz_buzz'
+
+describe 'fizz_buzz' do
+
+    it "returns 1 if number is 1" do
+        expect(fizz_buzz(1)).to eq 1 
+      end
+    
+      it "returns 'fizz' if number is divisible by 3" do
+        expect(fizz_buzz(6)).to eq 'fizz'
+      end
+    
+      it "returns 'buzz' if number is divisible by 5" do
+        expect(fizz_buzz(10)).to eq 'buzz'
+      end
+    
+      it "returns 'fizz buzz' if number is divisible by 15" do
+        expect(fizz_buzz(30)).to eq 'fizz buzz'
+      end
+end 
